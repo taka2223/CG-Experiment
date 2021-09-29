821384dd47a1c6d58cc066cfba339710b33b6ee5
win <z18933038283@163.com>
2021-09-29 12:20:42 +0800

    111

---
+++ exp1.cpp
@@ -0,0 +1,63 @@
+#include <GL/glut.h>
+#include <iostream>
+using namespace std;
+
+    float X1=250.0,Y1=500.0,X2=500.0,Y2=0.0;
+    void myinit()
+    {
+        // attributes
+        glClearColor(1.0,1.0,1.0,1.0);
+        glColor3f(1.0,0.0,0.0);
+        
+        // set up viewing
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluOrtho2D(0.0, 500.0,0.0,500.0);
+        glMatrixMode(GL_MODELVIEW);
+    }
+
+    void displaySG()
+    {
+        GLfloat vertices[3][3] = {{0.0,0.0,0.0}, {250.0, 500.0,0.0}, {500.0,0.0,0.0}};
+        if ((Y1!=Y2||Y1!=0.0||Y2!=0.0)&&(X1!=X2||X1!=0.0||X2!=0.0))
+        {
+            vertices[1][0]=X1;
+            vertices[1][1]=Y1;
+            vertices[2][0]=X2;
+            vertices[2][1]=Y2;
+        }
+        else cout<<"3 vertexes are in the same line."<<endl;
+        // an arbitrary triangle in the plane z = 0;
+        GLfloat p[3] = {7.5, 5.0, 0.0};
+                    // or set any desired initial point which is inside the triangle;
+        int  j,k;
+        int total_points = 150000;
+        glBegin(GL_POINTS);
+        for (k = 0; k < total_points; k++)
+        {
+                    /* pick a random vertex from 0, 1, 2*/
+                    j = rand()%3;
+                    // compute new location;
+            p[0] = (p[0]+vertices[j][0])/2;
+            p[1] = (p[1]+vertices[j][1])/2;
+                    // display new point
+                        glVertex3fv(p);
+        }
+        glEnd();
+        glFlush();
+    }
+    int  main(int argc, char **argv)
+    {   
+        cout<<"Please input the coordinates of the two vertexs: "<<endl;
+        cin>>X1>>Y1>>X2>>Y2;
+            
+        glutInit(&argc, argv);
+        glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+        glutInitWindowSize(500,500);
+        glutInitWindowPosition(50,50);
+        glutCreateWindow("Sierpinski Gasket");
+        glutDisplayFunc(displaySG);
+        myinit();
+        glutMainLoop();
+        return 0;
+    }
---
+++ exp3.cpp
@@ -0,0 +1,125 @@
+#include <GL/glut.h>
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+void myinit()
+{
+    // attributes
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    // set up viewing
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+int N, M, count = 1;
+int seq[5][6] = { 0 };
+int dir[4][2] = {
+    { 0, 1 },
+    { 0, -1 },
+    { 1, 0 },
+    { -1, 0 }
+};
+void Number(int x0, int y0)
+{
+    if (x0 >= 0 && x0 <= 4 && y0 >= 0 && y0 <= 5) {
+        if (seq[x0][y0] == 0) {
+
+            seq[x0][y0] = count++;
+            for (size_t i = 0; i < 4; i++) {
+                int ran = rand() % 4;
+                Number(x0 + dir[ran][0], y0 + dir[ran][1]);
+            }
+        }
+    }
+}
+
+void erase()
+{
+    int x0 = rand() % 5;
+    int y0 = rand() % 6;
+    Number(x0, y0);
+    for (size_t i = 0; i < 5; i++) {
+        for (size_t j = 0; j < 6; j++) {
+            cout << seq[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    glColor3f(0.0f, 0.0f, 0.0f);
+    for (size_t i = 0; i < 5; i++) {
+        for (size_t j = 0; j < 5; j++) {
+            if (abs(seq[i][j] - seq[i][j + 1]) == 1) {
+                cout << "draw:" << i << " , " << j << endl;
+                glBegin(GL_LINES);
+                glVertex2i((i + 1) * 70 + 1, j * 70 + 1);
+                glVertex2i((i + 1) * 70 + 1, (j + 1) * 70 + 1);
+                glEnd();
+            }
+        }
+    }
+    for (size_t i = 0; i < 6; i++) {
+        for (size_t j = 0; j < 4; j++) {
+            if (abs(seq[j][i] - seq[j + 1][i]) == 1) {
+                cout << "draw:" << i << " , " << j << endl;
+                glBegin(GL_LINES);
+                glVertex2i(i * 70 + 1, (j + 1) * 70 + 1);
+                glVertex2i((i + 1) * 70 + 1, (j + 1) * 70 + 1);
+                glEnd();
+            }
+        }
+    }
+    glFlush();
+}
+void displaySG()
+{
+    glColor3f(1.0f, 0.0, 0.0);
+    glBegin(GL_LINES);
+    glLineWidth(2.0f);
+    int x_ini = 1, y_ini = 1, temp_y = y_ini, temp_x = x_ini;
+    for (size_t i = 0; i < N; i++) {
+        /* draw the row */
+        glVertex2i(x_ini, temp_y);
+        glVertex2i(x_ini + 70 * M, temp_y);
+
+        temp_y += 70;
+    }
+    for (size_t i = 0; i < M; i++) {
+        /* column */
+        glVertex2i(temp_x, y_ini);
+        glVertex2i(temp_x, temp_y);
+        temp_x += 70;
+    }
+    glVertex2i(x_ini, temp_y);
+    glVertex2i(temp_x, temp_y);
+
+    glVertex2i(temp_x, temp_y);
+    glVertex2i(temp_x, y_ini);
+
+    glEnd();
+    erase();
+    glBegin(GL_LINES);
+    glVertex2i(20, 1);
+    glVertex2i(40, 1);
+
+    glVertex2i(300, 1);
+    glVertex2i(320, 1);
+    glEnd();
+    glFlush();
+}
+int main(int argc, char** argv)
+{
+    N = 5, M = 6;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("Sierpinski Gasket");
+    glutDisplayFunc(displaySG);
+    myinit();
+    glutMainLoop();
+    return 0;
+}
